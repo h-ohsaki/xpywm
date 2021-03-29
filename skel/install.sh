3076bccaed80603db94789ac9cff57f8d94f6eee
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2021-03-29 19:49:15 +0900

    add skelton files

---
+++ skel/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt install -y xbase-clients rxvt-unicode xfonts-terminus net-tools redshift python3-pip
+sudo pip3 install xpywm xpymon xpylog
+
+cd $HOME
+for i in .Xdefaults .emacs .xinitrc
+do
+    wget -O $i http://www.lsnl.jp/~ohsaki/software/xpywm/skel$i
+done
+
+#sudo apt install build-essential linux-headers-4.19.0-9-amd64
